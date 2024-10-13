cff95548a4d88ffe97552669803dddff05988358
eedu7 <mueedahmad067@gmail.com>
2024-10-13 14:55:00 +0500

    MINOR: code refactor

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from pydantic_settings import BaseSettings
 
+
 class Config(BaseSettings):
-    MYSQL_URL: str | None  = os.getenv('MYSQL_URL')
-    JWT_SECRET_KEY: str | None = os.getenv('JWT_SECRET_KEY')
-    JWT_ALGORITHM: str | None = os.getenv('JWT_ALGORITHM')
+    MYSQL_URL: str | None = os.getenv("MYSQL_URL")
+    JWT_SECRET_KEY: str | None = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM: str | None = os.getenv("JWT_ALGORITHM")
 
 
 config: Config = Config()
