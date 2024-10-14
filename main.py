1e0c800cb5a1c7871b92e4ba99f28eae876a0c6e
eedu7 <mueedahmad067@gmail.com>
2024-10-14 08:37:32 +0500

    FEAT: added the main.py for server and app for fastapi app

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import argparse
 
+import uvicorn
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, help="Port", default=8000)
+    parser.add_argument("--host", help="Host", default="127.0.0.1")
+
+    args = parser.parse_args()
+
+    uvicorn.run("api:app", host=args.host, port=args.port, reload=True)
+
+
+if __name__ == "__main__":
+    main()
--- route/app.py
+++ route/app.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
 
+app: FastAPI = FastAPI(
+    title="Image Processing Service",
+    description="Image Processing Service",
+    version="1.0.0",
+
+)
