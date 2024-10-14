bd126ca03a052dd29e91d58321f091f947b827a7
eedu7 <mueedahmad067@gmail.com>
2024-10-14 08:35:47 +0500

    FEAT: added the new project structure and created the db and config

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from pydantic_settings import BaseSettings
 
 
-class Config(BaseSettings):
-    MYSQL_URL: str | None = os.getenv("MYSQL_URL")
-    JWT_SECRET_KEY: str | None = os.getenv("JWT_SECRET_KEY")
-    JWT_ALGORITHM: str | None = os.getenv("JWT_ALGORITHM")
+class ConfigSettings(BaseSettings):
+    MYSQL_URL: str
+    SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"  # Add a default value if necessary
+    JWT_EXPIRY: int = 60 * 24 * 7  # 1 week expiry by default
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 
-config: Config = Config()
+config: ConfigSettings = ConfigSettings()
