f26ba59b13b69a05727f759085e8eb18c9e4dd78
eedu7 <mueedahmad067@gmail.com>
2024-10-21 11:18:54 +0500

    MINOR: fixed file_name

--- routes/image.py
+++ routes/image.py
@@ -1,8 +1,13 @@
 import time
 
-from fastapi import APIRouter, File, UploadFile, HTTPException, status
+from fastapi import APIRouter, File, UploadFile, HTTPException, status, Depends
 import boto3
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from config import config
+from crud.image import ImageCrud
+from db import get_async_session
+
 router: APIRouter = APIRouter()
 
 s3 = boto3.client(
@@ -18,15 +23,20 @@ async def get_all_images(skip: int = 0, limit: int = 100):
 
 
 @router.post("/")
-async def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...), session: AsyncSession = Depends(get_async_session)):
     try:
+        image_crud: ImageCrud = ImageCrud(session)
         file_name = str(str(time.time()).split(".")[0] + str(file.filename))
         s3.upload_fileobj(
             file.file, config.S3_BUCKET, file_name,
         )
+        await image_crud.create({
+            "name": file_name,
+        })
+        file_url: str = f"https://{config.S3_BUCKET}.s3.{config.AWS_REGION}.amazonaws.com/{file_name}"
         return {
             "message": "Image uploaded successfully!",
-            "file_name": file_name,
+            "file_url": file_url,
         }
 
     except Exception as e:
@@ -34,3 +44,4 @@ async def upload_image(file: UploadFile = File(...)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Error uploading image: {e}"
         )
+
