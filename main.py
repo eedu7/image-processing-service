bb5b13ae228344df007067c1486c2144771e5e48
eedu7 <mueedahmad067@gmail.com>
2024-10-21 11:19:07 +0500

    FEAT: added the optional host and port

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import argparse
+
 import uvicorn
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, help="Port", default=8000)
+    parser.add_argument("--host", help="Host", default="127.0.0.1")
+
+    args = parser.parse_args()
+
+    uvicorn.run("routes:app", host=args.host, port=args.port, reload=True)
+
+
 if __name__ == "__main__":
-    uvicorn.run(
-        "routes:app",
-        host="127.0.0.1",
-        port=8001,
-        reload=True,
-    )
+    main()
