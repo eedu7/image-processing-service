a84b7a76a60e389b609f4da4a569742e76bf740a
eedu7 <mueedahmad067@gmail.com>
2024-11-01 15:55:39 +0500

    updated the config.py file, to contain the env variable for mysql

--- core/config.py
+++ core/config.py
@@ -1,16 +1,21 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
 class ConfigSettings(BaseSettings):
     class Config:
         case_sensitive = True
-        env_file = "../.env"
+        env_file = Path(__file__).parent / "../.env"
         env_file_encoding = "utf-8"
 
 
 class Config(ConfigSettings):
     HOST: str = "127.0.0.1"
     PORT: int = 8000
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_DATABASE: str
 
 
 config: Config = Config()
