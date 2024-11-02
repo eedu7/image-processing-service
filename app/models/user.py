c3d5824154ad18e8c8e5e78edc415ee123f6cba1
eedu7 <mueedahmad067@gmail.com>
2024-11-02 18:33:54 +0500

    Added the user model, using sqlalchemy

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+import uuid
+
+from sqlalchemy import Boolean, String
+from sqlalchemy.dialects.mysql import CHAR
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.database import Base
+from core.database.mixins import TimestampMixin
+
+
+class User(Base, TimestampMixin):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        CHAR(36), primary_key=True, default=uuid.uuid4, unique=True, nullable=False
+    )
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
