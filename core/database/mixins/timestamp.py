7be0be6c50ca9c9e6d09466ca8bd7b558462e2da
eedu7 <mueedahmad067@gmail.com>
2024-11-02 18:33:40 +0500

    Updated the timestamp.py by sqlalchemy latest practices

--- core/database/mixins/timestamp.py
+++ core/database/mixins/timestamp.py
@@ -1,20 +1,22 @@
+from typing import Optional
+
 from sqlalchemy import DateTime, func
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import MappedColumn
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
     """Mixin class to add created_at and updated_at timestamps to models."""
 
     @declared_attr
-    def created_at(cls) -> MappedColumn:
+    def created_at(cls) -> Mapped[Optional[DateTime]]:
         """Timestamp for when the record was created."""
-        return MappedColumn(DateTime, default=func.now(), nullable=False)
+        return mapped_column(DateTime, default=func.now(), nullable=False)
 
     @declared_attr
-    def updated_at(cls) -> MappedColumn:
+    def updated_at(cls) -> Mapped[Optional[DateTime]]:
         """Timestamp for when the record was last updated."""
 
-        return MappedColumn(
+        return mapped_column(
             DateTime, default=func.now(), nullable=False, onupdate=func.now()
         )
