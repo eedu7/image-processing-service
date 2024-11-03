a8279ad372b2eecd1766417dd472a53ef535b036
eedu7 <mueedahmad067@gmail.com>
2024-11-03 09:45:08 +0500

    MINOR: added method to hash password and verify the hashed password

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,3 +1,6 @@
 from .database_config import get_database_url
+from .password_handler import hash_password, verify_password
 
-__all__ = ["get_database_url"]
+__all__ = ["get_database_url",
+           "hash_password",
+           "verify_password"]
