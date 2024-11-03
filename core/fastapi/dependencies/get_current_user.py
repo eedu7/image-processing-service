c22cd8eccdac374f3cea0d43e32c1250a02bd02b
eedu7 <mueedahmad067@gmail.com>
2024-11-03 11:20:27 +0500

    FEAT: added the token authentication and get current user method

---
+++ core/fastapi/dependencies/get_current_user.py
@@ -0,0 +1,11 @@
+from fastapi import Depends, Request
+
+from app.crud.user import UserCRUD
+from core.factory import Factory
+
+
+async def get_current_user(
+        request: Request,
+        user_crud: UserCRUD = Depends(Factory.get_user_crud)
+):
+    return await user_crud.get_by_id(request.user.id)
