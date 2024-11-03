8ec5df6f20e3f835a707aedf60faedca9191d114
eedu7 <mueedahmad067@gmail.com>
2024-11-03 09:49:14 +0500

    FEAT: Added the user request schema for registration of user

---
+++ app/schemas/requests/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class RegisterUser(BaseModel):
+    username: str
+    password: str
+    email: str
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "username": "admin",
+                "password": "<PASSWORD>",
+                "email": "<EMAIL>",
+            }
+        }
